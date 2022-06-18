e689c817fc8c2be48f1dbc27e27ee4ef27608fe9
Victor Bergamini <vbergdev@gmail.com>
2022-06-18 09:11:32 -0300

    calculator project

---
+++ src/components/Button.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Button.css";
+
+const Button = (props) => (
+  <button
+    onClick={(e) => props.click && props.click(props.label)}
+    className={`button
+  ${props.operation ? "operation" : ""}
+  ${props.double ? "double" : ""}
+  ${props.triple ? "triple" : ""}
+  `}
+  >
+    {props.label}
+  </button>
+);
+
+export default Button;
